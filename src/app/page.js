496da465c1c522201864e80f0077b15b5323db59
14Berhee <berhee14@gmail.com>
2024-10-21 03:56:32 +0800

    team-project

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+// import Image from "next/image";
+
+import Header from "../app/Components/Header"
+
+export default function Home() {
+  return (
+   
+    <div >
+      <Header/>
+ 
+    </div>
+  );
+}
+
